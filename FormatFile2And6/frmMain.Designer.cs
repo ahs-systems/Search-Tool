645cf5fd9c1999d4820380e4624241e610b3200d
AHS-Systems <AHS_Systems@outlook.com>
2017-11-03 15:38:01 -0600

    Add minimize button to the form of FormatType2and6

--- FormatFile2And6/frmMain.Designer.cs
+++ FormatFile2And6/frmMain.Designer.cs
@@ -36,16 +36,18 @@
             this.lblExit = new System.Windows.Forms.Label();
             this.btnFile2 = new Bunifu.Framework.UI.BunifuFlatButton();
             this.btnFile6 = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCurrentUser = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.830189F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(108, 51);
+            this.label1.Location = new System.Drawing.Point(99, 41);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.Size = new System.Drawing.Size(56, 21);
             this.label1.TabIndex = 3;
             this.label1.Text = "Zone: ";
             // 
@@ -59,7 +61,7 @@
             this.cboZone.Items = new string[] {
         "Edmonton",
         "NCS"};
-            this.cboZone.Location = new System.Drawing.Point(154, 42);
+            this.cboZone.Location = new System.Drawing.Point(154, 36);
             this.cboZone.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cboZone.Name = "cboZone";
             this.cboZone.NomalColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(128)))), ((int)(((byte)(30)))));
@@ -81,7 +83,7 @@
             this.lblExit.AutoSize = true;
             this.lblExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(123)))), ((int)(((byte)(29)))));
-            this.lblExit.Location = new System.Drawing.Point(444, 9);
+            this.lblExit.Location = new System.Drawing.Point(449, 10);
             this.lblExit.Name = "lblExit";
             this.lblExit.Size = new System.Drawing.Size(25, 24);
             this.lblExit.TabIndex = 5;
@@ -110,7 +112,7 @@
             this.btnFile2.IconVisible = true;
             this.btnFile2.IconZoom = 50D;
             this.btnFile2.IsTab = false;
-            this.btnFile2.Location = new System.Drawing.Point(37, 109);
+            this.btnFile2.Location = new System.Drawing.Point(37, 103);
             this.btnFile2.Name = "btnFile2";
             this.btnFile2.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(128)))), ((int)(((byte)(30)))));
             this.btnFile2.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(187)))), ((int)(((byte)(106)))), ((int)(((byte)(25)))));
@@ -121,7 +123,7 @@
             this.btnFile2.Text = "Format File 2";
             this.btnFile2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.btnFile2.Textcolor = System.Drawing.Color.White;
-            this.btnFile2.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 8.150944F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFile2.TextFont = new System.Drawing.Font("Century Gothic", 8.830189F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnFile2.Click += new System.EventHandler(this.btnFile2_Click);
             // 
             // btnFile6
@@ -146,7 +148,7 @@
             this.btnFile6.IconVisible = true;
             this.btnFile6.IconZoom = 50D;
             this.btnFile6.IsTab = false;
-            this.btnFile6.Location = new System.Drawing.Point(259, 109);
+            this.btnFile6.Location = new System.Drawing.Point(259, 103);
             this.btnFile6.Name = "btnFile6";
             this.btnFile6.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(128)))), ((int)(((byte)(30)))));
             this.btnFile6.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(187)))), ((int)(((byte)(106)))), ((int)(((byte)(25)))));
@@ -157,8 +159,32 @@
             this.btnFile6.Text = "Format Type 6 From File 1";
             this.btnFile6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.btnFile6.Textcolor = System.Drawing.Color.White;
-            this.btnFile6.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 8.150944F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFile6.TextFont = new System.Drawing.Font("Century Gothic", 8.830189F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnFile6.Click += new System.EventHandler(this.btnFile6_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(123)))), ((int)(((byte)(29)))));
+            this.label2.Location = new System.Drawing.Point(421, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 22);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "_";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblCurrentUser
+            // 
+            this.lblCurrentUser.Font = new System.Drawing.Font("Century Gothic", 8.150944F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrentUser.ForeColor = System.Drawing.Color.DimGray;
+            this.lblCurrentUser.Location = new System.Drawing.Point(211, 179);
+            this.lblCurrentUser.Name = "lblCurrentUser";
+            this.lblCurrentUser.Size = new System.Drawing.Size(265, 21);
+            this.lblCurrentUser.TabIndex = 9;
+            this.lblCurrentUser.Text = "Zone: asdfsadfs a";
+            this.lblCurrentUser.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblCurrentUser.Visible = false;
             // 
             // frmMain
             // 
@@ -166,6 +192,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(28)))), ((int)(((byte)(33)))));
             this.ClientSize = new System.Drawing.Size(481, 201);
+            this.Controls.Add(this.lblCurrentUser);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnFile6);
             this.Controls.Add(this.btnFile2);
             this.Controls.Add(this.lblExit);
@@ -190,6 +218,8 @@
         private System.Windows.Forms.Label lblExit;
         private Bunifu.Framework.UI.BunifuFlatButton btnFile6;
         private Bunifu.Framework.UI.BunifuFlatButton btnFile2;
+        private System.Windows.Forms.Label lblCurrentUser;
+        private System.Windows.Forms.Label label2;
     }
 }
 
